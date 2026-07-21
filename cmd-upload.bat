6658b393cb9330c76bf0f18fc022145fc261e7b0
for-fo-f-zsq <forfof.zsq@gmail.com>
2026-07-21 11:38:41 +0800

    update

--- cmd-upload.bat
+++ cmd-upload.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 for /f "tokens=*" %%i in ('powershell -Command "(Get-Content package.json | Select-String '\"version\"' | Select-Object -First 1).Line -replace '.*\"version\"\s*:\s*\"([^\"]+)\".*', '$1'"') do set VERSION=%%i
-git push origin main
 git tag v%VERSION%
 git push origin v%VERSION%
